9f3d5beb532b9f931457eb1ae274fed1c41eed55
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2024-05-22 15:18:14 +0530

    feat(ci): Inject Firebase env vars into Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+// Load environment variables from .env files
+import dotenv from 'dotenv';
+import process from 'process';
+dotenv.config();
+
 export default defineConfig({
   css: {
     preprocessorOptions: {
       less: {
         javascriptEnabled: true,
         modifyVars: {
-          // "@primary-color": "#3AAFA9",
-          "@primary-color": "#455A64",
-          "@font-family": "'Poppins', sans-serif",
-          "@font-size-base": "14px",
-          "@border-radius-base": "5px"
+          '@primary-color': '#455A64',
+          '@font-family': "'Poppins', sans-serif",
+          '@font-size-base': '14px',
+          '@border-radius-base': '5px'
         }
       }
     }
@@ -23,6 +26,16 @@ export default defineConfig({
   },
   define: {
     // Some libraries use the global object, even though it doesn't exist in the browser.
-    global: {}
+    global: {},
+    // Inject environment variables
+    'process.env': {
+      VITE_APP_FIREBASE_API_KEY: JSON.stringify(process.env.VITE_APP_FIREBASE_API_KEY),
+      VITE_APP_AUTH_DOMAIN: JSON.stringify(process.env.VITE_APP_AUTH_DOMAIN),
+      VITE_APP_FIREBASE_PROJECT_ID: JSON.stringify(process.env.VITE_APP_FIREBASE_PROJECT_ID),
+      VITE_APP_FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.VITE_APP_FIREBASE_STORAGE_BUCKET),
+      VITE_APP_FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID),
+      VITE_APP_FIREBASE_APP_ID: JSON.stringify(process.env.VITE_APP_FIREBASE_APP_ID),
+      VITE_APP_FIREBASE_MEASUREMENTID: JSON.stringify(process.env.VITE_APP_FIREBASE_MEASUREMENTID)
+    }
   }
 });
